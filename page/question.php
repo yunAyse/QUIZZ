12923d44274e5d0e953b6b7872227246320d616b
octador <mutuon.j@gmail.com>
2023-12-21 22:16:00 +0100

    start-process 2.0

--- page/question.php
+++ page/question.php
@@ -3,22 +3,20 @@
 require_once('../connexion/connect.php');
 include_once('../partials/header.php');
 include_once('../partials/footer.php');
-include_once('../process/question-process.php');
-
 include_once('../process/start-process.php');
 
 ?>
 <section class="text-center">
     <h1>Question</h1>
 
-    <p><?php echo $joinresponseAndQuestions['question'] ?></p>
+    <p><?php echo $resultQuestion ?></p>
     <div class="text-center">
         <!-- il envoie a response-process qui va compter le score et renvoie a question-process pour regene question-->
         <form action="../process/response-process.php" method="post">
 
-            <button type="submit" name="response1"><?php [$rand_keys[0]] ?></button>
-            <button type="submit" name="response2"><?php [$rand_keys[1]] ?></button>
-            <button type="submit" name="response3"><?php [$rand_keys[2]] ?></button>
+            <button type="submit" name="response1"><?php echo [$result[0]] ?></button>
+            <button type="submit" name="response2"><?php echo [$result[1]] ?></button>
+            <button type="submit" name="response3"><?php echo[$result[2]] ?></button>
 
         </form>
 
--- process/start-process.php
+++ process/start-process.php
@@ -1,3 +1,7 @@
+<p>e</p>
+<p>e</p>
+<p>e</p>
+
 <?php
 require_once('../connexion/connect.php');
 
@@ -6,22 +10,35 @@ $varId = 1;
 
 $request = $db->prepare("SELECT *  FROM question WHERE id = $varId");
 $request->execute();
-
 $questions = $request->fetchAll();
 
-foreach ($questions as $question) {
+$result =[];
 
-    var_dump($question['1']);
-
+foreach ($questions as $question) {   
+//  var_dump($question);
+    // var_dump($question['1']);
     $selectQuestion = $question['0'];
     $request = $db->prepare("SELECT *  FROM response WHERE question_id = $selectQuestion");
     $request->execute();
     $responses = $request->fetchAll();
 
     foreach ($responses as $response) {
-
-        var_dump($response['1']);
-
+        // var_dump($response['1']);
         $resultResponses =  $response['1'];
+        array_push($result, $resultResponses);
+    
     };
+} 
+$resultQuestion = $question['1'];
+
+$requestrandom = $db->query("SELECT* FROM response WHERE question_id = $selectQuestion ORDER BY RAND() ");
+$resultRandom = $requestrandom->fetchAll();
+$results = $resultRandom;
+foreach ($results as $result) {
+    $resultEchoRandom = $result['1'];
+
 }
+echo $resultEchoRandom;
+
+
+
