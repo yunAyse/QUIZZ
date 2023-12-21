22a18d5073b2fd91bf25c203ef2efd95563aa907
yunAyse <aysepc0@gmail.com>
2023-12-21 09:44:18 +0100

    index done

--- partials/header.php
+++ partials/header.php
@@ -8,4 +8,32 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
-<body>
+<body style="background-color: #e2e2e2">
+
+<header>
+    
+<nav style="background-color: #011936" class="navbar fixed-top" >
+  <div class="container-md">
+    <a class="navbar-brand fs-1 text-light" href="#">QUIZZ</a>
+    <button class="navbar-toggler bg-light" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="offcanvas offcanvas-end" style="background-color: #011936" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title text-light" id="offcanvasNavbarLabel">USERS</h5>
+        <button type="button" class="btn-close bg-light" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body text-light">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li class="nav-item">
+            <p>user</p>
+          </li>
+          <li class="nav-item">
+            user
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
+</header>
